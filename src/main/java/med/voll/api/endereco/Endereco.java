5757ad65e1f7627b6721598ca7d56f9a0b7862bc
Pedro Leite <contato.pedro77@gmail.com>
2025-01-09 11:02:21 -0300

    Cria a migration para criar a tabela medicos, adiciona a entidade medico e configuraçoes necessarias para salvar um medico via metodo post no banco de dados

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(EnderecoDto enderecoDto) {
+        this.logradouro = enderecoDto.logadouro();
+        this.bairro = enderecoDto.bairro();
+        this.cep = enderecoDto.cep();
+        this.numero = enderecoDto.numero();
+        this.complemento = enderecoDto.complemento();
+        this.cidade = enderecoDto.cidade();
+        this.uf = enderecoDto.uf();
+    }
+}
