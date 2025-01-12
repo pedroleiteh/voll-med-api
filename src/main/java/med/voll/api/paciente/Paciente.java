a56bca31bd60d4905f855b740cab85dea27e3c05
Pedro Leite <contato.pedro77@gmail.com>
2025-01-12 17:27:29 -0300

    Cria migrations para tabela paciente, a classe da entidade e repository para representar o paciente

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,42 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(PacientePostDto dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
