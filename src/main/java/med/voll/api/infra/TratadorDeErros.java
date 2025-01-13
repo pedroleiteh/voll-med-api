4f8316d4d9c732b637923da478f0a9535c0e8f7d
Pedro Leite <contato.pedro77@gmail.com>
2025-01-13 15:51:21 -0300

    Cria um metodo para tratar erro de validacao dos campos da classe

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,6 +1,10 @@
 package med.voll.api.infra;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +16,17 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<dadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(dadosErroValidacao::new).toList());
+    }
+
+    private record dadosErroValidacao(String campo, String msg) {
+        public dadosErroValidacao(FieldError err){
+            this(err.getField(), err.getDefaultMessage());
+        }
     }
 }
